48e86728deb3a694831b898216a9b818cb0a518c
Steve Katz <katz.stephenj@gmail.com>
2017-11-26 18:45:59 -0500

    Use vue component for app element

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import Vue from 'vue'
 
-const app = new Vue({
-  data: {
-    hello: 'ahoy matefooiwjef'
-  },
-  template: '<div id="app">{{ hello }}</div>'
+Vue.component('app', {
+  template: `
+    <div id="app">
+      <nav class="nav has-shadow">
+        <div class="container">
+          <a href="/">
+            <img src="http://bit.ly/vue-img"
+                 alt="Vue SPA" />
+          </a>
+        </div>
+      </nav>
+      <section class="main-section section"></section>
+      <footer class="footer">
+        <div class="container">
+          <div class="content has-text-centered">
+            Fooollow usss on <a href="https://twitter.com/bstavroulakis" target="_blank">Twitter</a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  `
+})
 
+const app = new Vue({
+  render: h => h('app')
 })
 
 export { app }
