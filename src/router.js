517800585d957ec47947a2ed8b3fa9e7731dcca1
Steve Katz <katz.stephenj@gmail.com>
2017-12-02 11:20:49 -0500

    Highlight navbar links when router-link elements are active

--- src/router.js
+++ src/router.js
@@ -6,6 +6,8 @@ import Login from './theme/Login.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+  mode: 'history',
+  linkActiveClass: 'is-active',
   routes: [
     { path: '/login', component: Login },
     { path: '/', component: Category }
--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,10 +1,15 @@
 <template>
-  <nav class="nav has-shadow">
+  <nav class="navbar has-shadow">
     <div class="container">
-      <a href="/">
-        <img src="http://bit.ly/vue-img"
-             alt="Vue SPA" />
-      </a>
+      <div class="navbar-brand" >
+        <router-link to="/">
+          <img src="http://bit.ly/vue-img"
+               alt="Vue SPA" />
+        </router-link>
+      </div>
+      <router-link class="navbar-item is-tab" to="/" exact>Home</router-link>
+      <router-link class="navbar-item is-tab" to="/login" exact>Login</router-link>
+      </div>
     </div>
   </nav>
 </template>
